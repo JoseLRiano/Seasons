4c14d19d975f7ba6c664af5c0c8ad0c5b022d792
JoseLRiano <joselriano@yahoo.com>
2020-05-05 01:34:07 -0400

    Updates

---
+++ precache-manifest.b902da567f20edad397289522cc9d059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9f09169c7acd08cd886c0d8aac19a5",
+    "url": "/Seasons/index.html"
+  },
+  {
+    "revision": "a2a820214c8a1a0b1c7a",
+    "url": "/Seasons/static/css/main.d8801335.chunk.css"
+  },
+  {
+    "revision": "fa354485be05596676f5",
+    "url": "/Seasons/static/js/2.af9bdbca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Seasons/static/js/2.af9bdbca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a820214c8a1a0b1c7a",
+    "url": "/Seasons/static/js/main.ba41b804.chunk.js"
+  },
+  {
+    "revision": "5a3069c864a68940ac2a",
+    "url": "/Seasons/static/js/runtime-main.cc53df28.js"
+  }
+]);
